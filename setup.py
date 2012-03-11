374e36ff1f6085ef7bfd1d732b187c3937fc1dbf
Sylvain Prat <sylvain.prat@gmail.com>
2012-03-11 21:14:55 +0100

    Added a README file The version string is now extracted from the package Added more metadata information

--- setup.py
+++ setup.py
@@ -1,23 +1,50 @@
-VERSION = '0.0.1'
+# -*- coding: UTF-8 -*-
+# Copyright (c) 2012 Sylvain Prat. This program is open-source software,
+# and may be redistributed under the terms of the MIT license. See the
+# LICENSE file in this distribution for details.
+
+import os
+import re
 
 from setuptools import setup, find_packages
 
+
+def read(*path_parts):
+    here = os.path.dirname(__file__)
+    return open(os.path.join(here, *path_parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+# package description
+desc = "ebookme is a Web application that provides a bookmarklet to convert any web page to a .mobi file"
+long_desc = read('README')
+
+
 setup(
-      name = 'ebookme',
-      version = VERSION,
-      author = '',
-      author_email = '',
-      description = '',
-      license = '',
-      keywords = '',
-      url = '',
-      packages = find_packages(),
-      include_package_data = True,
-      package_data = {'' : ['*.cfg']},
-      zip_safe = False,
-      install_requires = ('nagare',),
-      message_extractors = { 'ebook.me' : [('**.py', 'python', None)] },
-      entry_points = """
+      name='ebookme',
+      version=find_version('ebookme', '__init__.py'),
+      author='Sylvain Prat',
+      author_email='sylvain.prat+ebookme@gmail.com',
+      description=desc,
+      long_description=long_desc,
+      license='MIT License',
+      keywords='',
+      url='http://bitbucket.org/sprat/ebookme',
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={'' : ['*.cfg']},
+      zip_safe=False,
+      install_requires=('nagare',),
+      message_extractors={ 'ebook.me' : [('**.py', 'python', None)] },
+      entry_points="""
       [nagare.applications]
       ebookme = ebookme.app:app
       """
